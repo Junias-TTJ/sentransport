880caa07ca2f80155a91ab13158edd640ab58d96
Junie Tiamnang <juniedouce@gmail.com>
2026-05-02 14:18:35 +0200

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistiques.js
@@ -0,0 +1,12 @@
+import './Statistiques.css';
+
+function Statistiques() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">10</span>
+      <span className="stat-libelle">lignes</span>
+    </div>
+  );
+}
+
+export default Statistiques;
